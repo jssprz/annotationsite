706f739f909bec352621af86bcf81f3edd59d01e
Jesús Pérez-Martín <jperezmartin90@gmail.com>
2019-07-15 13:15:05 -0400

    fixes the annotation filter in tagger images selection

--- tweets/views.py
+++ tweets/views.py
@@ -77,7 +77,7 @@ def tagger(request):
         # - less than 10 annotations
         # - current user has never annotated
         user_annotated_medias_ids = Annotation.objects.filter(created_by=request.user).values_list('media_id', flat=True)
-        full_annotated_medias_ids = Annotation.objects.annotate(num_per_media=Count('media')).filter(num_per_media__lt=10).values_list('media_id', flat=True)
+        full_annotated_medias_ids = Annotation.objects.annotate(num_per_media=Count('media')).filter(num_per_media__gte=10).values_list('media_id', flat=True)
         excluded_medias_ids = list(user_annotated_medias_ids) + list(full_annotated_medias_ids)
         print(excluded_medias_ids)
         medias = TweetMedia.objects.filter(id__lte=50000).exclude(id__in=excluded_medias_ids).all()
